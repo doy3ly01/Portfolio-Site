657207db80d10fab4eaa1615b73fabb8eb7bfda4
doyley01 <joshdoyley01@gmail.com>
2025-01-11 14:15:20 +0000

    Setup angular routing, introduced some css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { GoalsComponent } from './goals/goals.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, NgIf, HomeComponent, RouterLink, RouterLinkActive, AboutComponent, ProjectsComponent, GoalsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-site';
+  currentRoute: string = ''; // Track active route
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // Subscribe to route changes and update currentRoute
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.activatedRoute.snapshot.firstChild?.routeConfig?.path || 'home-component';
+    });
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { GoalsComponent } from './goals/goals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home-component', component: HomeComponent},
+    {path: 'about-component', component: AboutComponent},
+    {path: 'projects-component', component: ProjectsComponent},
+    {path: 'goals-component', component:GoalsComponent}
+];
